b3709cad554ece1bc725ea783f57e6dcd88a232d
Zuzana Sommerová <zuzana.sommerova@mall.cz>
2020-03-26 15:36:06 +0100

    Create index.vue

---
+++ index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+
+    <p>
+      Adress:
+      <input type="text" v-model="address">
+    </p>
+    <AdressEditor v-model="address" />
+
+  </div>
+</template>
+
+<script>
+
+/*
+ÚKOL
+
+Vytvorte komponentu AddressEditor pre editáciu adresy, ktorú dostanete vo formáte:
+U garáží 1611/1
+
+Chceme zvlášť políčka pre ulicu, číslo popisné, číslo orientačné.
+*/
+
+import AdressEditor from './AdressEditor'
+
+export default {
+  components: { AdressEditor },
+  data: function () {
+    return {
+      address: 'U garáží 1611/1'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
